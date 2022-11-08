0d453381f31c71c3417b3b8884c33a5f8125ffde
CaglaCengiz <cigdem18701986@gmail.com>
2022-11-08 14:34:43 +0300

    8.11.2022

--- src/day1/Q07_IsDigit.java
+++ src/day1/Q07_IsDigit.java
@@ -10,4 +10,22 @@ public class Q07_IsDigit { /*
  Character.isDigit()
  Integer.valueOf()
 */
+    public static void main(String[] args) {
+        String str="J4\"/4 1$ 34$¥";
+        digitSum(str);
+      
+
+
+    }
+
+    private static void digitSum(String str) {
+        int toplam=0;
+        for (int i = 0; i < str.length(); i++) {
+            if(Character.isDigit(str.charAt(i))){
+                toplam+=Integer.valueOf(""+str.charAt(i));
+            }
+            
+        }
+        System.out.println("toplam = " + toplam);
+    }
 }
